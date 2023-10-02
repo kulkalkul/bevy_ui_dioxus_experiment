5440a1f8df99a404c1f2711b8609f5e17343fae5
Bora <yokunjon@gmail.com>
2023-10-02 10:36:13 +0300

    Add temporary binary for testing quickly

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use app_root::AppRootComponent;
+use bevy::{prelude::App, DefaultPlugins};
+
+mod plugin;
+mod integration;
+mod app_root;
+
+use dioxus::prelude::{Scope, Element, rsx};
+use plugin::DioxusPlugin;
+
+use dioxus::html as dioxus_elements;
+
+fn main() {
+    App::new()
+        .add_plugins((
+            DefaultPlugins,
+            DioxusPlugin,
+        ))
+        .insert_resource(AppRootComponent(app_root))
+        .run();
+}
+
+fn app_root(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div { "hello world" }
+    })
+}
