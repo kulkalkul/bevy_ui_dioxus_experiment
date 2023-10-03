a29ba85fcfb71d8fd15c282aca91852ac05f63c9
Bora <yokunjon@gmail.com>
2023-10-03 11:45:15 +0300

    Add elements crate

---
+++ crates/elements/src/elements.rs
@@ -0,0 +1,17 @@
+pub struct div;
+impl div {
+    pub const TAG_NAME: &'static str = "div";
+    pub const NAME_SPACE: Option<&'static str> = None;
+}
+
+pub struct img;
+impl img {
+    pub const TAG_NAME: &'static str = "img";
+    pub const NAME_SPACE: Option<&'static str> = None;
+}
+
+pub struct button;
+impl button {
+    pub const TAG_NAME: &'static str = "button";
+    pub const NAME_SPACE: Option<&'static str> = None;
+}
---
+++ crates/elements/src/lib.rs
@@ -0,0 +1,3 @@
+mod elements;
+
+pub use elements::*;
